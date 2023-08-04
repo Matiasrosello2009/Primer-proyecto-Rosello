4bdfd4adf62b18cd450260df9fb3e11bc0f98819
alumno <alumno@Instituto.local>
2023-08-04 07:52:48 -0300

    toma de notas js 3

--- clase1.js
+++ clase1.js
@@ -2,6 +2,7 @@ alert('Esto es una alerta de practica')
 alert('Aqui se muestra una notificacion con un texto')
 //comentario de practica// 
 console.log(50+51)
+//para entrar a la consola: entrar al "index", luego seleccionar "inspeccionar" y luego "console".// 
 
 let nombreAlumno = 'Matias' 
 nombreAlumno = nombreAlumno + ' Rosello'
@@ -10,4 +11,3 @@ nombreAlumno = nombreAlumno + ' Rosello'
 //asignar valor a variable: _nombre variable = ´valor a asignar´_ //
 //Tambien se puede asignar variable a la vez.// 
 //operaciones matematicas: suma = num + 30 (ejemplo)//
-
