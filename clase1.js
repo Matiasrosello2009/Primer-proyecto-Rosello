dbbda6fd6e86dcd719cde2304947a8e0d8c2e724
alumno <alumno@Instituto.local>
2023-08-04 07:42:02 -0300

    Update clase1.js

--- clase1.js
+++ clase1.js
@@ -1,3 +1,8 @@
 alert('Esto es una alerta de practica')
 alert('Aqui se muestra una notificacion con un texto')
+//comentario de practica// 
 
+//Let: nombre de variable// 
+//asignar valor a variable: _nombre variable = ´valor a asignar´_ //
+//Tambien se puede asignar variable a la vez.// 
+//operaciones matematicas: suma = num + 30 (ejemplo)//
