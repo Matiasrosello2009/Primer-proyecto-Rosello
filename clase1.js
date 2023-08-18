264679f4ad618a56309def8e092e4c47bf1f9aaf
alumno <alumno@Instituto.local>
2023-08-18 07:59:07 -0300

    practica js 5

--- clase1.js
+++ clase1.js
@@ -19,3 +19,47 @@ nombreAlumno = nombreAlumno + ' Rosello'
 let nombreAlumno 
 nombreAlumno = prompt ('ingresa tu nombre') 
 alert('Bienvenido '+ nombreAlumno)
+
+//trabajo practico 1 con javascrip//
+num = 4 
+
+console.log('la suma es', suma = num +6)
+console.log('La resta es', resta = num -6) 
+console.log('La multiplicacion es', multiplicacion = num *6)
+console.log('La division es', division = num /6) 
+
+num = 2
+
+console.log('la suma es', suma = num +5)
+console.log('La resta es', resta = num -5) 
+console.log('La multiplicacion es', multiplicacion = num *5)
+console.log('La division es', division = num /5) 
+
+alert (4+6)
+alert(4-6) 
+alert(4*6)
+alert(4/6) 
+
+alert (2+5)
+alert(2-5) 
+alert(2*5)
+alert(2/5) 
+
+alert ('Mi edad es '+(2023 - 2009))
+
+let = nombreAlumno
+let = apellidoAlumno
+
+nombreAlumno = prompt('ingresa tu nombre')
+apellidoAlumno = prompt('Ingresa tu apellido')
+alert('Bienvenido '+ nombreAlumno + apellidoAlumno)
+
+let = edadAlumno
+
+edadAlumno = prompt ('Ingresa el año en que naciste')
+
+let diez = 10
+let tres = 3
+let resultadoMultiplicacion = diez * tres 
+alert ('El resultado de la multiplicacion de 10x3 es: '+ resultadoMultiplicacion) 
+
