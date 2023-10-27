026d69f84f2f626a38b9d63103938b5ebc7cb8f5
alumno <alumno@Instituto.local>
2023-10-27 08:37:28 -0300

    practica js 29.9

---
+++ claseTrabajoPractico.js
@@ -0,0 +1,54 @@
+//TRABAJO PRACTICO//
+
+function saludar (nombre)
+let nombre = prompt('Escribe tu nombre normal') 
+{
+    if (nombre = Nahuel || Santino)
+{
+    alert('Hola. ')
+}
+
+else
+{
+    alert('Bienvenido amigo/a 👋')
+}
+}
+
+function mayorMenorEdad (edadUsuario)
+let edadUsuario = Number(prompt('Ingrese su edad '))
+{
+    if (edadUsuario <= 18)
+    {
+        alert('Sos mayor de edad. ')
+    }
+    else
+    {
+        alert('Sos menor de edad. ')
+    }
+}
+
+function etapaEdad (etapaUsuario)
+let etapaUsuario = Number(prompt('Ingrese su edad para saber en qué etapa de su vida se encuentra... '))
+{
+    if (etapaUsuario <= 1 && >= 6)
+    {
+        alert('Sos menor a un niño. ')
+    }
+    else if (etapaUsuario <=6 && > 12)
+    {
+        alert('Sos un/a niño/a común y corriente. ')
+    }
+    else if (etapaUsuario <=13 && > 18)
+    {
+        alert('Sos un/a adolescente. ')
+    }
+    else if (etapaUsuario <= 19 && > 26)
+    {
+        alert('Sos un/a joven. ')
+    }
+    else if (etapaUsuario <= 27 && > 59)
+    {
+        alert('Sos un adulto. 👍')
+    }
+    else if 
+}
