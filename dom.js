f1b8a94cdb1ee439fa87ce47c14e612b1be31d32
alumno <alumno@Instituto.local>
2023-11-03 09:05:54 -0300

    practica js 35

---
+++ dom.js
@@ -0,0 +1,4 @@
+//El DOM permite al usuario manipular la interfaz de la página. Para eso, debemos colocar por variable un elemento. Por ejemplo: "titulo" = h1. //
+let titulo = document.querySelector(h1)
+//Aquí se va a modificar el elemento de la forma deseada del usuario// 
+titulo.innerText = 'Hola :D' 
