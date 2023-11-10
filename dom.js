37efa2fa70372b6b6edfff7eeeabb0e0287b35fc
alumno <alumno@Instituto.local>
2023-11-10 07:58:39 -0300

    practica js 40.1

--- dom.js
+++ dom.js
@@ -1,11 +1,10 @@
 //👽El DOM permite al usuario manipular la interfaz de la página. Para eso, debemos colocar por variable un elemento. Por ejemplo: "titulo" = h1. //
 let titulo = document.querySelector(h1)
 titulo.innerText = 'Hola :D 👋👽' 
-//👆Aquí se va a modificar el elemento de la forma deseada del usuario// 
 let imagen = document.querySelector('.imagen')
 imagen.src = 'https://i.ytimg.com/vi/MvhWXSP58xE/sddefault.jpg' 
 
-//Practica con un boton 👇// 
+//Programacion de un boton 👇// 
 let button = document.querySelector('#CambiarImagen')
 button.onclick = function()
 {
