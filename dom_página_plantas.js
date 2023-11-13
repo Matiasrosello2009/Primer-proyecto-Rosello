2b8b933d29a070976c0247e13f1ced1755f6f191
alumno <alumno@Instituto.local>
2023-11-13 10:38:45 -0300

    practica js 43

---
+++ dom_página_plantas.js
@@ -0,0 +1,10 @@
+let buttonGirasol = document.querySelector('#Girasol')
+buttonGirasol.onclick = function (){
+    alert('Visualmente, el girasol posee un tallo grueso, largo y alto con una flor grande de petalos amarillos pequeños y en todo el centro contiene cemillas saladas comestibles, conocidas como pipas. ')
+}
+
+let buttonTulipanes = document.querySelector('#Tulipanes')
+buttonTulipanes.onclick = function (){
+    alert('Visualmente, los tulipanes son flores suaves, pequeñas de pétalos hermosos y no son muy comunes. Pueden ser de diferentes colores. Los más comunes son las flores rojas. ')
+}
+
