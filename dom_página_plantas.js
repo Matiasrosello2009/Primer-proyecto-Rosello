983bfa88b8934d6233588b492b039b3e15f8a395
alumno <alumno@Instituto.local>
2023-11-13 10:51:56 -0300

    practica js 44.1

--- dom_página_plantas.js
+++ dom_página_plantas.js
@@ -9,11 +9,11 @@ buttonTulipanes.onclick = function (){
 }
 
 let buttonHelecho = document.querySelector('#Helecho')
-buttonTulipanes.onclick = function (){
+buttonHelecho.onclick = function (){
     alert('Visualmente, el helecho es una hierba mediana que puede variar su tamaño, posee hojas grandes y verdes y ramas largas y finas. La parte central de su “raíz” (rizoma) se macera en vino blanco. Este vino de antojil se toma principalmente para ayudar a soldar roturas, trastornos de las articulaciones y dolores reumáticos.')
 }
 
 let buttonDienteLeon = document.querySelector('#DienteLeon')
-buttonTulipanes.onclick = function (){
+buttonDienteLeon.onclick = function (){
     alert('Visualmente, los dinetes de leon son pompones suaves de lo que llamamos como "panaderos" que son semillas que se vuelan con el viento o cuando los soplamos. Tienen un tallo liso y corto y, antes de que crezca ese típico pompom suave carácterístico de la planta, la misma es una flor amarrila de muchos pétalos largos y finos color amarillo. ')
 }
