4292d7c3c71ac7dfb91a8050f7e933d1b9c3ec47
alumno <alumno@Instituto.local>
2023-11-13 10:49:52 -0300

    practica js 44

--- dom_página_plantas.js
+++ dom_página_plantas.js
@@ -8,3 +8,12 @@ buttonTulipanes.onclick = function (){
     alert('Visualmente, los tulipanes son flores suaves, pequeñas de pétalos hermosos y no son muy comunes. Pueden ser de diferentes colores. Los más comunes son las flores rojas. ')
 }
 
+let buttonHelecho = document.querySelector('#Helecho')
+buttonTulipanes.onclick = function (){
+    alert('Visualmente, el helecho es una hierba mediana que puede variar su tamaño, posee hojas grandes y verdes y ramas largas y finas. La parte central de su “raíz” (rizoma) se macera en vino blanco. Este vino de antojil se toma principalmente para ayudar a soldar roturas, trastornos de las articulaciones y dolores reumáticos.')
+}
+
+let buttonDienteLeon = document.querySelector('#DienteLeon')
+buttonTulipanes.onclick = function (){
+    alert('Visualmente, los dinetes de leon son pompones suaves de lo que llamamos como "panaderos" que son semillas que se vuelan con el viento o cuando los soplamos. Tienen un tallo liso y corto y, antes de que crezca ese típico pompom suave carácterístico de la planta, la misma es una flor amarrila de muchos pétalos largos y finos color amarillo. ')
+}
