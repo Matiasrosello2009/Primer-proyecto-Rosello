82ebc35fcbce45e0fd441f14ddd15b71a23b7957
alumno <alumno@Instituto.local>
2023-11-17 09:10:30 -0300

    practica js 44

--- dom_página_plantas.js
+++ dom_página_plantas.js
@@ -2,18 +2,30 @@ let buttonGirasol = document.querySelector('#Girasol')
 buttonGirasol.onclick = function (){
     alert('Visualmente, el girasol posee un tallo grueso, largo y alto con una flor grande de petalos amarillos pequeños y en todo el centro contiene cemillas saladas comestibles, conocidas como pipas. ')
 }
+buttonGirasol.dblclick = function(){
+    alert('Hay distintos tipos de girasoles: oleaginosos, de confitura o confitería, de alto contenido de ácido oleico y ornamentales.El girasol contiene hasta un 58 % de aceite en su fruto, aceite usado para cocinar, y en la producción de biodiésel. El aceite de girasol virgen —obtenido del prensado de las pipas—, aunque no posee las cualidades del aceite de oliva, sí posee una cantidad cuatro veces mayor de vitamina E natural que este. El "orujo" que queda después de la extracción del aceite se utiliza como alimento para el ganado. Los tallos contienen una fibra que puede ser usada en la elaboración del papel, y las hojas pueden servir también de alimento para el ganado.')
+}
 
 let buttonTulipanes = document.querySelector('#Tulipanes')
 buttonTulipanes.onclick = function (){
     alert('Visualmente, los tulipanes son flores suaves, pequeñas de pétalos hermosos y no son muy comunes. Pueden ser de diferentes colores. Los más comunes son las flores rojas. ')
+}
+buttonTulipanes.dblclick = function(){
+    alert('El centro de diversidad del género se encuentra en las montañas Pamir e Hindu Kush en las estepas de Kazajistán,4​ encontrándose distribuido por Oriente Medio, Irán y Afganistán particularmente, hacia la península de Anatolia, Europa del Sur y norte de África, por el oeste, y hacia el noreste de China, por el Este.')
 }
 
 let buttonHelecho = document.querySelector('#Helecho')
 buttonHelecho.onclick = function (){
     alert('Visualmente, el helecho es una hierba mediana que puede variar su tamaño, posee hojas grandes y verdes y ramas largas y finas. La parte central de su “raíz” (rizoma) se macera en vino blanco. Este vino de antojil se toma principalmente para ayudar a soldar roturas, trastornos de las articulaciones y dolores reumáticos.')
 }
+buttonHelecho.dblclick = function(){
+    alert('Filogenéticamente se puede dividir en dos grupos: Ophioglossidae (ofioglosales y psilotales) y el grupo conocido popularmente como de los "helechos verdaderos" (maratiales y polipódidos) que hoy en día, tras análisis moleculares de ADN, se ha determinado que forman un clado (grupo monofilético según la escuela cladista). Algunas especies, como el helecho águila (Pteridium aquilinum) o la azolla (Azolla filiculoides) son extremadamente comunes y se encuentran repartidas por toda la superficie terrestre.')
+}
 
 let buttonDienteLeon = document.querySelector('#DienteLeon')
 buttonDienteLeon.onclick = function (){
     alert('Visualmente, los dinetes de leon son pompones suaves de lo que llamamos como "panaderos" que son semillas que se vuelan con el viento o cuando los soplamos. Tienen un tallo liso y corto y, antes de que crezca ese típico pompom suave carácterístico de la planta, la misma es una flor amarrila de muchos pétalos largos y finos color amarillo. ')
+}
+buttonDienteLeon.dblclick = function(){
+    alert('En algunos periodos de escasez, la raíz seca se ha utilizado como sustituto de la achicoria, que a su vez es sustituto del café. Sus hojas silvestres o cultivadas son comestibles, se prefieren las que son jóvenes y tiernas para ensaladas, mientras que las maduras, al ser más amargas, se consumen cocidas, aunque está sin confirmarse la existencia de cultivos para este fin.')
 }
